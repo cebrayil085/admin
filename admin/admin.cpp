53c5b2d4b91e516ebee8a5d184b1a5d0c4640c77
cebrayil085 <resadd302@gmail.com>
2024-10-15 18:43:06 +0400

    createQuiz elave olundu

--- admin/admin.cpp
+++ admin/admin.cpp
@@ -1,10 +1,51 @@
-
-
 #include <iostream>
+#include <string>
+#include <limits>
+
 using namespace std;
-int main()
-{
-    
+
+const int MAX_QUIZZES = 100;
+const int MAX_OPTIONS = 4;
+
+struct Quiz {
+    string title;
+    string questions[MAX_QUIZZES];
+    string options[MAX_QUIZZES][MAX_OPTIONS];
+    int answers[MAX_QUIZZES];
+    int questionCount;
+};
+
+Quiz quizzes[MAX_QUIZZES];
+int quizCount = 0;
+
+void createQuiz() {
+    cout << "Enter quiz title: ";
+    cin >> quizzes[quizCount].title;
+
+    cout << "How many questions? ";
+    cin >> quizzes[quizCount].questionCount;
+
+    for (int i = 0; i < quizzes[quizCount].questionCount; ++i) {
+        cout << "Enter question " << (i + 1) << ": ";
+        cin >> quizzes[quizCount].questions[i];
+
+        for (int j = 0; j < MAX_OPTIONS; ++j) {
+            cout << "Enter option " << (j + 1) << ": ";
+            cin >> quizzes[quizCount].options[i][j];
+        }
+
+        cout << "Enter correct answer (1-4): ";
+        cin >> quizzes[quizCount].answers[i];
+    }
+
+    quizCount++;
+    cout << "Quiz created!\n";
 }
 
 
+
+
+
+int main() {
+    
+}
